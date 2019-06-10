c575be08d12dc7d743972142b1726847c9f002c7
Andy Dearing <andy.dearing@gmail.com>
2019-06-10 19:15:50 -0400

    can't seem to get the math right

--- histogram.py
+++ histogram.py
@@ -1,6 +1,6 @@
-def create_histogram(file, band_size):
+def create_histogram(file, band_number):
     nums = get_nums_from_file(file)
-    bands = get_bands(nums, band_size)
+    bands = get_bands(nums, band_number)
     print_histogram(bands)
 
 
@@ -12,12 +12,18 @@ def get_nums_from_file(file):
     return nums
 
 
-def get_bands(nums, band_size):
+def get_bands(nums, band_number):
     bands = {}
+    min_num = min(nums)
+    max_num = max(nums)
+    band_size = max_num - min_num / band_number
+    for index in range(band_number):
+        bands[(index * band_size) + min_num] = 0
     for num in nums:
-        band = num // band_size * band_size
+        band = num // band_size * band_size + min_num
         if band not in bands:
-            bands[band] = 0
+            breakpoint()
+            # bands[band] = 0
         bands[band] += 1
     return bands
 
@@ -32,14 +38,14 @@ if __name__ == "__main__":
     from pathlib import Path
 
     parser = argparse.ArgumentParser(description='Create a histogram')
-    parser.add_argument('-b', '--band-size', default=10, type=float)
+    parser.add_argument('-b', '--band-number', default=10, type=float)
     parser.add_argument('file', help='file to read')
     args = parser.parse_args()
 
     file = Path(args.file)
 
     if file.is_file():
-        create_histogram(file, args.band_size)
+        create_histogram(file, args.band_number)
     else:
         print(f"{file} does not exist!")
         exit(1)
