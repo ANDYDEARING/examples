24525da4b468b0f32746bb96b9d714e314bdcf12
Andy Dearing <andy.dearing@gmail.com>
2019-06-11 15:13:35 -0400

    works by the midpoint method, found small rounding error in the code on line 38

--- histogram.py
+++ histogram.py
@@ -24,27 +24,48 @@ def get_bands(nums, band_number):
     bands = {}
     min_num = min(nums)
     max_num = max(nums)
-    band_size = (max_num - min_num) / band_number
+
+    # uses the midpoint method to include both minimum and maximum
+    # dividing by band number - 1 because half a range is appended to each end
+    if band_number != 1:
+        band_size = (max_num - min_num) / (band_number - 1)
+    else:
+        band_size = 0
+
+    # make the histogram with ranges
     for index in range(band_number):
-        bands[(index * band_size) + min_num] = 0
+        # bands[(index * band_size) + min_num] = 0
+        min_of_band = (index*band_size) + min_num - (band_size/2)
+        max_of_band = (index*band_size) + min_num + (band_size/2)
+        bands[(min_of_band,max_of_band)] = 0
+    
+    # add the numbers to the histogram
     for num in nums:
-        band = num // band_size * band_size + min_num
-
-        # only the maximum number is excluded
-        if band in bands:
-            bands[band] += 1
-
-    # accounts for the special case of the max number to include it
-    sorted(bands.keys(), reverse=True)[0] += 1
-
+        if find_band(num, bands) is None:
+            breakpoint()
+        bands[find_band(num, bands)] += 1
+        
     return bands
 
+def find_band(num, ranges_dict):
+    """accepts a dictionary with tuple keys of potential ranges and 
+    returns the key where the num belongs, returning None if not 
+    found"""
+
+    for range_tuple in ranges_dict:
+        if range_tuple[0] <= num < range_tuple[1]:
+            # if the range is found, exit funcion
+            correct_range_tuple = range_tuple
+            return correct_range_tuple
+
+    # if the range isn't found, return none
+    return None
 
 def print_histogram(bands):
     for band in sorted(bands.keys()):
         # limits decimal places to 1
-        trunc_band = int(band*10)/10
-        print(f"{str(trunc_band).rjust(6)} | {'*' * bands[band]}")
+        band_range = f"{round(band[0],1)}-{round(band[1],1)}"
+        print(f"{band_range.rjust(15)} | {'*' * bands[band]}")
 
 
 if __name__ == "__main__":
