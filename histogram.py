73a7503dd599c5520f8d64396663cd2149f0d523
Andy Dearing <andy.dearing@gmail.com>
2019-06-10 20:02:47 -0400

    works, now inclusive of both sides

--- histogram.py
+++ histogram.py
@@ -1,10 +1,14 @@
 def create_histogram(file, band_number):
+    """takes a file and a desired number of bands, prints
+    the histogram, and returns nothing"""
     nums = get_nums_from_file(file)
     bands = get_bands(nums, band_number)
     print_histogram(bands)
 
 
 def get_nums_from_file(file):
+    """reads numbers from a passed file and returns a list
+    of stripped strings converted to floats"""
     nums = []
     with open(file) as fh:
         for line in fh.readlines():
@@ -13,18 +17,25 @@ def get_nums_from_file(file):
 
 
 def get_bands(nums, band_number):
+    """accepts a list of floats and a desired number of bands,
+    returning a dictionary of bands and their frequencies. To 
+    maximize even data representation, both the maximum and 
+    minimum values are included as endpoints"""
     bands = {}
     min_num = min(nums)
     max_num = max(nums)
-    band_size = max_num - min_num / band_number
+    band_size = (max_num - min_num) / band_number
     for index in range(band_number):
         bands[(index * band_size) + min_num] = 0
     for num in nums:
         band = num // band_size * band_size + min_num
-        if band not in bands:
-            breakpoint()
-            # bands[band] = 0
-        bands[band] += 1
+
+        # accounts for the special case of the max number to include it
+        # in the highest data bucket as the upper limit
+        if (band not in bands) and (band - band_size in bands):
+            bands[band-band_size] += 1
+        else:
+            bands[band] += 1
     return bands
 
 
